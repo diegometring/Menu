fda8c95b7ca0fc188bae473dd629b84c23b43e7e
diegometring <diegometringteixeira@gmail.com>
2025-03-17 19:21:35 -0300

    .

--- src/main/java/com/example/menu/repository/ClientRepository.java
+++ src/main/java/com/example/menu/repository/ClientRepository.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface ClientRepository extends JpaRepository <Client, Long> {
     void deleteById(@NotNull Long id);
