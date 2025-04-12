29207b94203d402c34be3b00adcdf2d06e673b01
diegometring <diegometringteixeira@gmail.com>
2025-04-11 21:56:14 -0300

    .

--- src/main/java/com/example/menu/services/client/ClientService.java
+++ src/main/java/com/example/menu/services/client/ClientService.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,19 +23,21 @@ public class ClientService implements IClientService {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Client createClient(ClientRequestDTO data) {
-        Client clientt = clientRepository.findByEmail(data.email());
-
-        if (clientt == null) {
-            Client cli = new Client();
-            cli.setName(data.name());
-            cli.setPhoneNumber(data.phoneNumber());
-            cli.setEmail(data.email());
-            return clientRepository.save(cli);
-        } else {
+        if (clientRepository.findByEmail(data.email()) != null) {
             throw new RuntimeException("email already in use");
         }
+
+        Client cli = new Client();
+        cli.setName(data.name());
+        cli.setPhoneNumber(data.phoneNumber());
+        cli.setEmail(data.email());
+        cli.setPassword(passwordEncoder.encode(data.password()));
+        return clientRepository.save(cli);
     }
 
     @Override
@@ -43,15 +46,18 @@ public class ClientService implements IClientService {
             var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
             var auth = this.authenticationManager.authenticate(usernamePassword);
 
-            return clientRepository.findByEmail(data.email());
+            Client client = clientRepository.findByEmail(data.email());
+            if(client == null) {
+                throw new RuntimeException("Client not found");
+            }
+            return client;
         } catch (Exception e) {
-            return null;
+            throw new RuntimeException("authentication failure");
         }
     }
 
     @Override
     public List<Client> getAllClient() {
-        //criar try/catch para garantir que o findAll será recebido
         return clientRepository.findAll();
     }
 
